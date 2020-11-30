2f9dc6715f1c1a2c90a771bb9791fbd32a087360
tyautyau56 <i19yanagi@gmail.com>
2020-12-01 01:18:20 +0900

    complete cli

--- .bin/create-next-template.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-
-/*TODO:  make command line tool via node.js. under this line*/
---
+++ .bin/template-select.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+//TODO: make command line tool via node
+
+const inquirer = require('inquirer');
+const { execSync, spawnSync} = require("child_process");
+
+let select_template_url = null;
+let select_package_manager = null;
+let select_project_name = null;
+
+const detail = [
+    {
+        "name": "next-template",
+        "url": "https://github.com/tyautyau56/next-template.git"
+    },
+    {
+        "name": "react-template",
+        "url": "https://github.com/tyautyau56/react-template.git"
+    },
+    {
+        "name": "wasm-template",
+        "url": null
+    }
+]
+
+inquirer.prompt([
+    {
+        name: "dir_name",
+        message: "What's your project name?"
+    },
+    {
+        name: "select_template",
+        type: "list",
+        message: "Choose the template you want to use.",
+        choices: ["next-template", "react-template", "wasm-template"]
+    },
+    {
+        name: "select_package",
+        type: "list",
+        message: "Choose the package manager you want to use.",
+        choices: ["yarn", "npm"]
+    }
+]).then(({dir_name, select_template, select_package})=> {
+    let match = detail.filter(function (item, index) {
+        if (item.name === select_template) return true;
+    });
+    select_template_url = match[0].url;
+    select_package_manager = select_package;
+    select_project_name = dir_name;
+});
+
